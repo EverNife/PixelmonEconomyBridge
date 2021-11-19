a144692019c86421461217aefe3f1810432adfe2
Petrus <petrus_ant@hotmail.com>
2021-11-18 21:29:32 -0300

    Create a simple cache to prevent several updates at the same time

--- src/main/java/br/com/finalcraft/pixelmoneconomybridge/listener/PlayerListenerGlobal.java
+++ src/main/java/br/com/finalcraft/pixelmoneconomybridge/listener/PlayerListenerGlobal.java
@@ -3,6 +3,7 @@ package br.com.finalcraft.pixelmoneconomybridge.listener;
 import br.com.finalcraft.evernifecore.listeners.base.ECListener;
 import br.com.finalcraft.evernifecore.util.FCScheduller;
 import br.com.finalcraft.finaleconomy.api.events.EconomyUpdateEvent;
+import br.com.finalcraft.finaleconomy.config.data.FEPlayerData;
 import br.com.finalcraft.pixelmoneconomybridge.PixelmonEconomyBridge;
 import com.pixelmonmod.pixelmon.Pixelmon;
 import com.pixelmonmod.pixelmon.comm.packetHandlers.clientStorage.UpdateClientPlayerData;
@@ -13,22 +14,50 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.scheduler.BukkitRunnable;
 
+import java.util.HashSet;
+
 public class PlayerListenerGlobal implements ECListener {
 
-    @EventHandler(ignoreCancelled = false, priority = EventPriority.MONITOR)
-    public void onEconomyUpdateEvent(EconomyUpdateEvent event) {
+    private HashSet<FEPlayerData> PLAYERS_THAT_REQUIRED_UPDATE = new HashSet<>();
 
-        FCScheduller.runAssync(() -> {
-            if (event.getPlayerData().isPlayerOnline()){
-                EntityPlayerMP player = FMLCommonHandler.instance().getMinecraftServerInstance().getPlayerList().getPlayerByUUID(event.getPlayerData().getUniqueId());
-                UpdateClientPlayerData updateClientPlayerData = new UpdateClientPlayerData(event.getPlayerData().getMoneyWrapper().intValue());
-                Pixelmon.network.sendTo(updateClientPlayerData, player);
+    @Override
+    public void onRegister() {
+
+        new BukkitRunnable(){
+            @Override
+            public void run() {
+
+                final HashSet<FEPlayerData> oldSet;
+                synchronized (PLAYERS_THAT_REQUIRED_UPDATE){
+                    if (PLAYERS_THAT_REQUIRED_UPDATE.size() == 0){
+                        return;
+                    }
+                    oldSet = PLAYERS_THAT_REQUIRED_UPDATE;
+                    PLAYERS_THAT_REQUIRED_UPDATE = new HashSet<>();
+                }
+
+                FCScheduller.runSync(() -> {
+                    for (FEPlayerData playerData : oldSet) {
+                        EntityPlayerMP player = FMLCommonHandler.instance().getMinecraftServerInstance().getPlayerList().getPlayerByUUID(playerData.getUniqueId());
+                        if (player != null){
+                            UpdateClientPlayerData updateClientPlayerData = new UpdateClientPlayerData(playerData.getMoneyWrapper().intValue());
+                            Pixelmon.network.sendTo(updateClientPlayerData, player);
+                        }
+                    }
+                });
             }
-        });
+        }.runTaskTimerAsynchronously(PixelmonEconomyBridge.instance, 20,20);
 
     }
 
-    @EventHandler(ignoreCancelled = false, priority = EventPriority.MONITOR)
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void onEconomyUpdateEvent(EconomyUpdateEvent event) {
+        synchronized (PLAYERS_THAT_REQUIRED_UPDATE){
+            PLAYERS_THAT_REQUIRED_UPDATE.add(event.getPlayerData());
+        }
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR)
     public void onPlayerLoginEvent(PlayerLoginEvent event) {
         new BukkitRunnable(){
             @Override
